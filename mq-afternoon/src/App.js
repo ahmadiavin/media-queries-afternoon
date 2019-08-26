269dd97a32ff630ab5b700c27720d610f325b498
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-08-26 13:36:33 -0500

    initial commit

--- mq-afternoon/src/App.js
+++ mq-afternoon/src/App.js
@@ -1,70 +1,62 @@
 import React, { Component } from "react";
-import "./reset.css";
 import "./App.css";
-// import {MdDehaze} from 'react-icons/md'
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      menuStatus: "hidden"
+      menuClass: "linkContainer"
     };
+    this.toggleClass = this.toggleClass.bind(this);
   }
-  
-  toggleMenu = () => {
-    if(this.state.menuStatus === 'hidden-open') {
-      this.setState({menuStatus: 'hidden-close'});
-    } else {
-      this.setState({menuStatus: 'hidden-open'});
-    }
+  toggleClass(key, value) {
+    this.setState({ [key]: value });
   }
   render() {
     return (
-      <body>
-        <nav className="mainNav">
-          <div className="container">
-            <a className="bootstrap" href="#page-top">
-              Start Bootstrap
-            </a>
-
-            <button className='toggler' onClick={this.toggleMenu}>
-              Menu <i />
-            </button>
-
-            <div className="hidden">
-              <ul>
-                <li>
-                  <a className='nav-link' href="#Services">Services</a>
-                </li>
-                <li>
-                  <a className='nav-link' href="#Portfolio">Portfolio</a>
-                </li>
-                <li>
-                  <a className='nav-link' href="#About">About</a>
-                </li>
-                <li>
-                  <a  className='nav-link' href="#Team">Team</a>
-                </li>
-                <li>
-                  <a  className='nav-link' href="#Contact">Contact</a>
-                </li>
-              </ul>
-            </div>
+      <div className="outerContainer">
+        <nav>
+          <h1 className="homeLogo">Start Bootstrap</h1>
+          <button
+            className="menu"
+            onClick={() =>
+              this.state.menuClass === "linkContainer"
+                ? this.toggleClass("menuClass", "dropDown")
+                : this.toggleClass("menuClass", "linkContainer")
+            }
+          >
+            MENU &#x2630;{" "}
+          </button>
+          <div className={this.state.menuClass}>
+            <span id="services" className="links">
+              SERVICES
+            </span>
+            <span id="portfolio" className="links">
+              PORTFOLIO
+            </span>
+            <span id="about" className="links">
+              ABOUT
+            </span>
+            <span id="team" className="links">
+              TEAM
+            </span>
+            <span id="contact" className="links">
+              CONTACT
+            </span>
           </div>
         </nav>
-        <div className={this.state.menuStatus}></div>
-        <header>
-          <div className="headerContainer">
-            <div className="intro-text">
-              <div className="welcome">Welcome To Our Studio!</div>
-              <div className="nice">It's Nice To Meet You</div>
-              <a className="tellme" href="#services">
-                
-                Tell Me More
-              </a>
+        <div className="mainBody">
+          <div>
+            <div id="subHead" className="greeting">
+              Welcome To Our Studio!
             </div>
+            <div id="mainGreeter" className="greeting">
+              IT'S NICE TO MEET YOU
+            </div>
+            <button className="moreButton">TELL ME MORE</button>
           </div>
-        </header>
-      </body>
+        </div>
+      </div>
     );
   }
 }
